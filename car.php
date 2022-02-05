3341a6a6971ebf60bad113d39ba6f56495a7557f
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-02-05 13:17:51 +0100

    Prime modifiche allo stile. Hover su title, e img.

--- car.php
+++ car.php
@@ -1,6 +1,5 @@
 <?php
 require_once __DIR__ . '/server/db.php';
-
 // in questa pagina verranno stampate tutte le info della specifica car
 var_dump($db[$_GET['index']]['Marca']);
 ?>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <title>Document</title>
   <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
   <div id="app">
@@ -15,10 +15,9 @@
       <section class="car"
       v-for='(car,index) in cars_info'
       :key='`car-${index}`'>
-      <!-- al click apri una pagina nuova ... target='_blank' solo per test per evitare avanti indietro continui -->
-        <h2><a :href="`car.php?index=${index}`" target="_blank">{{car.fullName}}</a></h2>
-        <img :src="`img/${car.imgPath}`" alt="Immagine non trovata">
-        <p><b>Prezzo: </b>{{car.price}}&euro;</sup></p>
+      <h2><a :href="`car.php?index=${index}`">{{car.fullName}}</a></h2>
+      <img :src="`img/${car.imgPath}`" alt="Immagine non trovata" class="car-image" :title="car.fullName">
+        <p><b>Prezzo: </b>{{car.price.toFixed(2)}} &euro;</sup></p>
         <p><b>Cilindrata: </b>{{car.displacement}} cm<sup>3</sup></p>
         <p><b>Potenza: </b>{{car.cv}} CV</p>
         <p><b>Alimentazione: </b>{{car.supply}}</p>
